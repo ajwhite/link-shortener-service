99260e2519906353d2aa092133c7a75bfac4d7c3
Atticus White <contact@atticuswhite.com>
2017-05-07 15:48:08 -0400

    env fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import * as Routes from './routes';
 
+const PORT = process.env.LINK_SHORTENER_PORT || 8080;
+
 const app = express();
 app.use((req, res, next) => {
   req.body = '';
@@ -11,4 +13,6 @@ app.use((req, res, next) => {
 app.post('/create', Routes.createLink);
 app.get('/:shortcode', Routes.openShortcode);
 
-app.listen(8080);
+app.listen(PORT, () => {
+  console.log(`starting link-shortening-service on :${PORT}`);
+});
